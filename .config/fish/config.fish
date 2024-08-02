28af9b5ffa205cf88a9330ae9d4332211aa349c3
Jakub Prus <jakubprus@tuta.io>
2024-08-02 10:34:45 +0200

    dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias dnf="dnf5"
+alias v="nvim"
+alias l="eza --icons --git"
+alias ll="eza --icons --git --long --all"
